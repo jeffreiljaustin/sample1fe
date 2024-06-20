ecfca9ed99f8c3d78c31af900671f41a523436b2
jeffreiljaustin <jeffreilbelarmino@gmail.com>
2024-06-20 12:11:21 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,67 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('sample1'),
-        ),
-        backgroundColor: Colors.pink[200],
-      ),
-      body: Center(
-        child: Image.network('https://media.assettype.com/daily-tribune%2Fimport%2Fwp-content%2Fuploads%2F2023%2F09%2FMaki-PR-Photo.jpg?w=1200&auto=format%2Ccompress&fit=max'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.pink[200],
-        child: Icon(
-            Icons.add,
-            color: Colors.white,
-        ),
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    backgroundColor: Colors.white70,
+    appBar: AppBar(
+      title: Text('User Profile'),
+      backgroundColor: Colors.pink[200],
+      centerTitle: true,
+    ),
+    body: Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'NAME',
+            style: TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            'Jeffreil Austin R. Belarmino',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 25.0,
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'YEAR',
+            style: TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            'Third',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 25.0,
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'EMAIL',
+            style: TextStyle(
+              fontSize: 16.0,
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            'jeffreilbelarmino@gmail.com',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 25.0,
+            ),
+          )
+        ],
       ),
     ),
-  ));
-}
-
+  ),
+));
